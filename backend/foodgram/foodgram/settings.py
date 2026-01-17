5f3eff23ac251944e0c538dddf82c46d385daeb1
pavel-2009 <ms.radionova@bk.ru>
2026-01-17 13:33:12 +0500

    f

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'app/staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # → /app/backend/staticfiles
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
